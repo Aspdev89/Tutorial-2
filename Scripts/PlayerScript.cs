b4fa765e22c7d292969a39c7ec71e96b5d524c65
Aspdev89 <89466285+Aspdev89@users.noreply.github.com>
2021-10-20 16:10:11 -0400

    Add files via upload

--- Scripts/PlayerScript.cs
+++ Scripts/PlayerScript.cs
@@ -11,13 +11,41 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
 
+    public Text livesText;
+    public Text winText;
+    public Text loseText;
     private int scoreValue = 0;
+    private int livesValue = 3;
+    public bool gameOver;
+
+public AudioClip musicClipOne;
+
+public AudioClip musicClipTwo;
+
+public AudioSource musicSource;
+
+Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        livesText.text = livesValue.ToString();
+         winText.text = "";
+        loseText.text = "";
+        gameOver = false;
+
+        {
+
+          musicSource.clip = musicClipOne;
+
+          musicSource.Play();
+          musicSource.loop = true;
+
+          anim.SetInteger("State", 1);
+
+         }
     }
 
     // Update is called once per frame
@@ -41,7 +69,53 @@ public class PlayerScript : MonoBehaviour
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
+        if (scoreValue == 4) 
+                        {
+                          transform.position = new Vector3(33.0f, 0.0f);
+                          livesValue = 3; 
+                          livesText.text = livesValue.ToString();
+                        }
+                        
+         if(scoreValue== 8)
+          {
+              gameOver = true;
+               scoreValue += 1; scoreValue = 9;
+                    score.text = scoreValue.ToString();
+          }
+          //if(scoreValue==8)
+          //{
+           if(gameOver == true)
+          {
+            winText.text = "You Win! Game by Joey DeSimone";
+            livesValue = 99;
+            livesText.text = livesValue.ToString();
+             //Destroy(gameObject);
+              {
+                  musicSource.Stop();
+                musicSource.clip = musicClipTwo;
 
+                musicSource.Play();
+                gameOver = false;
+                scoreValue += 1; scoreValue = 9;
+                    score.text = scoreValue.ToString();
+
+         }
+         }//}
+      
+    
+
+    if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            livesText.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+         if(livesValue<= 0)
+          {
+            loseText.text = "You Lose!";
+            Destroy(gameObject);
+         }
+      
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -50,7 +124,7 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
             }
         }
     }
